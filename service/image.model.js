e67973ff8a1452d6a2a1d4c95e4dd06fc3cd32a9
Yatnish manik <77111329+Yatnishmanik@users.noreply.github.com>
2026-07-13 13:48:01 +0530

    Fix formatting of originalname field in imageSchema

--- service/image.model.js
+++ service/image.model.js
@@ -2,7 +2,7 @@
 const mongoose= require('mongoose')
 const imageSchema = mongoose.Schema({
     filename:{type:String},
-    originalname:{type:String},
+    originalname:{type:String}, 
     size:{type:Number}
     
 })
